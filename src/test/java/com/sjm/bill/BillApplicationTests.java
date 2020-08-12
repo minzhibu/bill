204722ce6ef9e88b2eb0de042b7aefc12a4721ae
tom_glb <321261949_glb@qq.com>
2020-08-12 22:12:28 +0800

    构建项目

---
+++ src/test/java/com/sjm/bill/BillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjm.bill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
