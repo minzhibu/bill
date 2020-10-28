cd36e138c1095981032d6dbe77d2f405a7cb20cb
tom_glb <321261949_glb@qq.com>
2020-10-28 22:30:34 +0800

    登录成功

--- src/main/java/com/sjm/bill/controller/LoginController.java
+++ src/main/java/com/sjm/bill/controller/LoginController.java
@@ -1,10 +1,13 @@
 package com.sjm.bill.controller;
 
 import com.sjm.bill.common.CommonResult;
+import com.sjm.bill.mbg.model.UserInformation;
 import com.sjm.bill.server.LoginService;
 import org.springframework.web.bind.annotation.*;
 
-
+/**
+ * 登陸
+ */
 @RestController
 @RequestMapping("/login")
 public class LoginController {
@@ -15,13 +18,22 @@ public class LoginController {
         this.loginService = loginService;
     }
 
-    @GetMapping("/user")
-    public CommonResult loginToUser(@RequestParam("accountNumber")String accountNumber,@RequestParam("password") String password){
-        String token = loginService.LoginToUser(accountNumber, password);
+    @PostMapping("/user")
+    public CommonResult loginToUser(@RequestBody UserInformation userInformation){
+        String token = loginService.LoginToUser(userInformation.getAccountNumber(), userInformation.getPassword());
         if(token == null){
             return CommonResult.unauthorized(null);
         }else{
             return CommonResult.success(token);
+        }
+    }
+    @PostMapping("/token")
+    public CommonResult loginToToken(@RequestHeader("Authorization") String authorization){
+        UserInformation userInformation = loginService.LoginToToken(authorization);
+        if(userInformation == null){
+            return CommonResult.unauthorized(null);
+        }else{
+            return CommonResult.success(userInformation);
         }
     }
 }
