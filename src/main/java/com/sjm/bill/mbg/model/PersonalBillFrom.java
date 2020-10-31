c0f103867d389d7fc550fa345f03b98216115a6d
SJM <321261949@qq.com>
2020-10-31 10:35:45 +0800

    提交代码

--- src/main/java/com/sjm/bill/mbg/model/PersonalBillFrom.java
+++ src/main/java/com/sjm/bill/mbg/model/PersonalBillFrom.java
@@ -1,22 +1,26 @@
 package com.sjm.bill.mbg.model;
 
+import lombok.Data;
+import lombok.ToString;
+
 import java.io.Serializable;
 import java.util.Date;
-
+@Data
+@ToString
 public class PersonalBillFrom implements Serializable {
     /**
      * 主键
      *
      * @mbggenerated
      */
-    private Long id;
+    private String id;
 
     /**
      * 购买物品的名称
      *
      * @mbggenerated
      */
-    private Long name;
+    private String name;
 
     /**
      * 单价
@@ -62,86 +66,5 @@ public class PersonalBillFrom implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getName() {
-        return name;
-    }
-
-    public void setName(Long name) {
-        this.name = name;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-
-    public Long getMainId() {
-        return mainId;
-    }
-
-    public void setMainId(Long mainId) {
-        this.mainId = mainId;
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", name=").append(name);
-        sb.append(", price=").append(price);
-        sb.append(", amount=").append(amount);
-        sb.append(", mainId=").append(mainId);
-        sb.append(", label=").append(label);
-        sb.append(", createTime=").append(createTime);
-        sb.append(", updateTime=").append(updateTime);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
-    }
 }
